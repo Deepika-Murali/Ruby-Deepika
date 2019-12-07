718771d84e43a8ac1ee21bc319e87cda40a2379f
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-07 18:04:18 +0530

    Add files via upload
    Puts Keyword

---
+++ EX-1.rb
@@ -0,0 +1,13 @@
+#Start of comments
+#
+#Ex:1
+#puts keyword
+#Printing simple statements
+#End of comments
+#
+#Start of the code
+#
+puts "Puts is a keyword in Ruby"
+puts "It is used to display or print the output."
+#
+#End of the code
