357c0fec89a593f3e95fe5fdec2cfb39992cdb05
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-08 20:48:38 +0530

    Arithmetic Operators

---
+++ EX-3a.rb
@@ -0,0 +1,17 @@
+#Start of comments
+#
+#Ex:3a
+#Arithmetic Operators
+#End of comments
+#
+#Start of the code
+#
+puts "Sum of 4 and 14 is #{4+14}."
+#
+puts "Difference between 4 and 14 is #{14-4}."
+#
+puts "Quotient of 14 divided by 4 is #{14/4}."
+#
+puts "Remainder of 14 divided by 4 is #{14%4}."
+#
+puts "Product of 14 and 4 is #{14*4}."
