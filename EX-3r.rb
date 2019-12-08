529973d8e68a26c5cd1760d84afa6ee3670a624d
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-08 21:50:45 +0530

    Relational Operators

---
+++ EX-3r.rb
@@ -0,0 +1,16 @@
+#Start of comments
+#
+#Ex:3r
+#Relational Operators
+#End of comments
+#
+#Start of the code
+#
+puts "Is 4 greater than 14 ? #{4>14}."
+#
+puts "Is 14 greater than or equal to 14 ? #{14>=14}."
+#
+puts "Is 4 less than 14 ? #{4<14}."
+#
+puts "Is 14 less than or equal to 14 ? #{14<=14}."
+#
