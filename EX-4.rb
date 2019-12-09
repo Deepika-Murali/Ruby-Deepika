6ac38d0e157a31142017c00b82aebc95eb5bcb4e
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-09 14:57:10 +0530

    Variables

---
+++ EX-4.rb
@@ -0,0 +1,18 @@
+#Start of comments
+#
+#Ex:4
+#Variables
+#End of comments
+#
+#Start of the code
+#
+a = 5;
+b = 2
+puts "Area of a square with side #{a} is #{a * a}."
+#
+puts "Perimeter of a square with side #{a} is #{4 * a}."
+#
+puts "Area of a Rectangle with sides #{a} & #{b} is #{a * b}."
+#
+puts "Perimeter of a Rectangle with sides #{a} & #{b} is #{2*(a + b)}."
+#
