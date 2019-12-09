e4c9c7be92a2d8a15b03c245ae2a9f70b74c7626
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-09 19:35:39 +0530

    strings

---
+++ EX-6.rb
@@ -0,0 +1,15 @@
+#Start of comments
+#
+#Ex:6
+#Strings
+#End of comments
+#
+#Start of the code
+#
+Num_st = 28 # Number of states
+Num_ut = 9  # Number of Union Territories
+Str_1 = "There are #{Num_st} states "
+Str_2 = "and #{Num_ut} Union Territories in India."
+#
+puts Str_1 + Str_2
+#
