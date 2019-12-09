8570c83226fb073b2b3e21a4ae240b308b6941c5
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-09 18:23:15 +0530

    More Variables

---
+++ EX-5.rb
@@ -0,0 +1,19 @@
+#Start of comments
+#
+#Ex:5
+#More Variables
+#End of comments
+#
+#Start of the code
+#
+Emp_id = 185361  # Number
+Emp_name = 'Deepika R'  # Name
+Emp_LOC = 'Chennai' # City
+Emp_sal = 4.5 # lakhs
+#
+puts "#{Emp_name} was an employee of TCS #{Emp_LOC}."
+#
+puts "Her employee ID was #{Emp_id}."
+#
+puts "Her CTC was #{Emp_sal}LPA."
+#
