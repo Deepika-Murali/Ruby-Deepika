8a16f8791968290ccf4f1f7c1f2b32254ce897d9
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-10 16:12:53 +0530

    Print vs Put

---
+++ Ex-7.rb
@@ -0,0 +1,15 @@
+#Start of comments
+#
+#Ex:7
+#Print Vs Puts
+#End of comments
+#
+#Start of the code
+#
+V_dot = '.'
+#Format:1
+print "List of Programming languages:"
+puts "C,COBOL,RUBY" + V_dot * 10
+#Format:2
+puts "List of Programming languages:"
+print "C,COBOL,RUBY" + V_dot * 10
