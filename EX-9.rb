7af44fe4f73a3e54d00ce63d3e097fdb410a9779
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-11 10:33:09 +0530

    \n and %q{}

---
+++ EX-9.rb
@@ -0,0 +1,17 @@
+#Start of comments
+#
+#Ex:9
+#Usage of \n and %q{}
+#End of comments
+#
+#Start of the code
+#
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+puts days
+puts months
+puts %q{There are 7 days in week
+There are 12 months in a year
+There are 365 days in a normal year
+There are 366 days in a leap year
+}
