76ca20d98375a22f45f62d734a59fac993abe337
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-11 21:54:10 +0530

    gets.chomp

---
+++ Ex-11.rb
@@ -0,0 +1,13 @@
+#Start of comments
+#
+#Ex:11
+#Prompting for input(gets.chomp)
+#End of comments
+#
+#Start of the code
+#
+print "How old are you?"
+age = gets.chomp  #Enter your Usage
+print "How tall are you?"
+tall = gets.chomp  #Enter your height
+puts " So, you are #{age} years old and #{tall}."
