bca6b923e728a30d4160f224cd62337cd7a2ec99
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-11 21:39:23 +0530

    Escape sequences and """

---
+++ Ex-10.rb
@@ -0,0 +1,23 @@
+#Start of comments
+#
+#Ex:9
+#Escape Sequences and triple quotes
+#End of comments
+#
+#Start of the code
+#
+height_1 = "Ramu is 5'9\"."    # \"
+#
+height_2 = 'Raja is 5\'6".'    # \'
+#
+tab = "\t Paragrah Starts"    # \t
+#
+puts height_1
+puts height_2
+puts tab
+puts """
+There are 7 days in week \\
+There are 12 months in a year \\
+There are 365 days in a normal year \\
+There are 366 days in a leap year
+"""  # """ and \\
