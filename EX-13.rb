70b1522597957063a50cfb00ac520665ecc45491
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-12 20:46:38 +0530

    Parameters(ARGV) and Input($stdin.gets.chomp)

---
+++ EX-13.rb
@@ -0,0 +1,14 @@
+#Start of comments
+#
+#Ex:13
+#Parameters(ARGV) and Input($stdin.gets.chomp)
+#End of comments
+#
+#Start of the code
+#
+height,weight = ARGV  #Parameter Variables
+#
+print "How old are you?"
+age = $stdin.gets.chomp  #Enter a number
+
+puts " So, you are #{age} years old,#{weight} kgs and #{height} feet tall. "
