73d07429efb21b7f124b86837eefb53ffb588639
Deepika-Murali <58597378+Deepika-Murali@users.noreply.github.com>
2019-12-15 15:11:20 +0530

    Functions-2

---
+++ ex-19.rb
@@ -0,0 +1,21 @@
+#Start of comments
+#
+#Ex:19
+#Function-2
+#End of comments
+#
+#Start of the code
+#
+def product_num (a,b)
+  print "The product of #{a} and #{b} is "
+  print "#{a*b} "
+  print"\n"
+end
+#
+product_num(10,12)
+first_number = 14
+second_number = 15
+product_num(first_number,second_number)
+product_num(first_number+1,second_number+1)
+product_num(first_number*2,second_number*2)
+#
